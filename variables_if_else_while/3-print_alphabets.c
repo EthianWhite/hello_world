422889d8dab18eec08e7ded42ec01dc83107ce02
Ethian <5214@holbertonschool.com>
2022-06-17 13:48:09 -0700

    another loop

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Start Point
+ *
+ * Return: Remain 0
+ */
+
+int main(void)
+{
+char y;
+
+for (y = 'a'; y <= 'z'; y++)
+{
+putchar(y);
+}
+for (y = 'A'; y <= 'Z'; y++)
+{
+putchar(y);
+}
+putchar('\n')
+return (0);
+}
