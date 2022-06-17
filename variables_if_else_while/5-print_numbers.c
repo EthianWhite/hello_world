766f1e9ce06b524be9bcab80e9716b347925001c
Ethian <5214@holbertonschool.com>
2022-06-17 14:19:13 -0700

    a loop free of restrictions

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Start point
+ *
+ * Return: Remain 0
+ */
+int main(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
