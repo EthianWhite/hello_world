fe71639f2d19e7a68f944f583e3b5e89a529d8fc
Ethian <5214@holbertonschool.com>
2022-06-17 14:34:44 -0700

    Another loop of the alphabet but this one is in reverse

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Start point
+ *
+ * Return: Remain 0
+ */
+int main(void)
+{
+char u;
+
+for (u = 'z'; u >= 'a'; u++)
+{
+putchar(u);
+}
+putchar('\n');
+return (0);
+}
