f68c65b2fc1d05d60fb7c61733decbf8dcae96a5
Ethian <5214@holbertonschool.com>
2022-06-17 13:38:05 -0700

    let do ittt another loop

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio>
+
+/**
+ * main - The start
+ *
+ * Return: remain 0
+ */
+int main(void)
+{
+char x;
+
+for (x = 'a'; x <= 'z'; x++)
+{
+putchar(x);
+}
+putchar('\n');
+return (0);
+}
