893db78bbf88ae6a4c47a164014467d6b5b31c99
Ethian <5214@holbertonschool.com>
2022-06-17 14:29:57 -0700

    another loop but it has again restrictions

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Start point
+ *
+ * Return: Remain 0
+ */
+int main(void)
+{
+int o;
+
+for (o = 0; o < 10; o++)
+{
+putchar(o + '0');
+}
+putchar('\n');
+return (0);
+}
