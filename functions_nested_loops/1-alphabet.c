8c834ebd1157133111af6eda25cf5750e1ca57ed
Ethian <5214@holbertonschool.com>
2022-06-17 17:34:34 -0700

    Lets print the alphabet... again...

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Start Point
+ *
+ * Return: Remain 0
+ */
+void print_alphabet(void)
+{
+char x;
+
+for (x = 'a'; x <= 'z'; x++)
+{
+_putchar(x);
+}
+_putchar('\n');
+return (0);
+}
