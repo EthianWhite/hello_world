b6db7a84823dc628afb0742021c4a23eebcfd8d9
Ethian <5214@holbertonschool.com>
2022-06-17 19:42:17 -0700

    the addition of two number

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Start Point
+ *
+ * Return: The additions of the numbers
+ *
+ * @x: first number
+ * @y: second number
+ */
+int add(int x, int y)
+{
+return (x + y);
+}
