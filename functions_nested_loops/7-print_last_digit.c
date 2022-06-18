7efa649d1ad410129515b0166330c928066f78bc
Ethian <5214@holbertonschool.com>
2022-06-17 18:45:29 -0700

    Lets see what is the last digit of a number

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Start Point
+ *
+ * Return: The last digit of a number
+ *
+ * @x: The int
+ */
+int print_last_digit(int x)
+{
+int i = x % 10;
+
+if (n < 0)
+{
+n = -n;
+}
+if (i < 0)
+{
+i = -i;
+}
+_putchar(i + '0');
+return (i);
+}
