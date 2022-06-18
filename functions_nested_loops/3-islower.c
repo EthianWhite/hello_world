b82c6555a47cbc1ff23d4612869efb2ad008ed4e
Ethian <5214@holbertonschool.com>
2022-06-17 17:54:43 -0700

    Lets check if it is a lower case

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _islower - Start point
+ *
+ * Return: checks if it is lowercase
+ *
+ * @c: The character in question
+ */
+int _islower(int c)
+{
+return (c >= 'a' && c <= 'z');
+}
