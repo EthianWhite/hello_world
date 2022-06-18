2a3ebfb2d591b9ab20b169905d3ccc816f9d524e
Ethian <5214@holbertonschool.com>
2022-06-17 18:14:31 -0700

    lests see if it is greates, less or equal to zero

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - Start Point
+ *
+ * Return: checks if its greater, less or equal to zero
+ *
+ * @n: The number in questions
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+}
